9ede30297ffe2f0668a67aa2394dae347f8d131d
Dana Frostig <dana.frostig@gmail.com>
2019-11-30 19:06:43 -0500

    add rest of the sound files to script

--- assign8/assets/main.js
+++ assign8/assets/main.js
@@ -11,23 +11,52 @@ function getChordValue() {
 // var mySound;
 var cSound;
 var aSound;
+var gSound;
+var eSound;
+var dSound;
+var fSound;
+var amSound;
+var dmSound;
+var emSound;
+
 function preload() {
-  soundFormats('mp3', 'ogg');
-  // mySound = loadSound('assets/guitar_chords_all.mp3');
-  cSound = loadSound('assets/cChordSound.mp3');
-  aSound = loadSound('assets/aChordSound.mp3');
-  // console.log(isLoaded());
+    soundFormats('mp3', 'ogg');
+    // mySound = loadSound('assets/guitar_chords_all.mp3');
+    cSound = loadSound('assets/cChordSound.mp3');
+    aSound = loadSound('assets/aChordSound.mp3');
+    gSound = loadSound('assets/gChordSound.mp3');
+    eSound = loadSound('assets/eChordSound.mp3');
+    dSound = loadSound('assets/dChordSound.mp3');
+    fSound = loadSound('assets/fChordSound.mp3');
+    amSound = loadSound('assets/amChordSound.mp3');
+    dmSound = loadSound('assets/dmChordSound.mp3');
+    emSound = loadSound('assets/emChordSound.mp3');
+    // console.log(isLoaded());
 }
 
 function setup() {
-  // mySound.setVolume(0.5);
-  // mySound.play();
+    // mySound.setVolume(0.5);
+    // mySound.play();
 
-  cSound.setVolume(0.5);
-  cSound.play();
+    cSound.setVolume(0.5);
+    // cSound.play();
 
-  aSound.setVolume(0.5);
-  aSound.play();
+    aSound.setVolume(0.5);
+    // aSound.play();
+
+    gSound.setVolume(0.5);
+
+    eSound.setVolume(0.5);
+
+    dSound.setVolume(0.5);
+
+    fSound.setVolume(0.5);
+
+    amSound.setVolume(0.5);
+
+    dmSound.setVolume(0.5);
+
+    emSound.setVolume(0.5);
 }
 
 
@@ -43,24 +72,31 @@ function displayChord() {
     }
     else if (x =="gchord"){
         document.getElementById("frets").src = "./assets/imgs/gchord.png";
+        gSound.play();
     }
     else if (x =="echord"){
         document.getElementById("frets").src = "./assets/imgs/echord.png";
+        eSound.play();
     }
     else if (x =="dchord"){
         document.getElementById("frets").src = "./assets/imgs/dchord.png";
+        dSound.play();
     }
     else if (x =="fchord"){
         document.getElementById("frets").src = "./assets/imgs/fchord.png";
+        fSound.play();
     }
     else if (x =="amchord"){
         document.getElementById("frets").src = "./assets/imgs/amchord.png";
+        amSound.play();
     }
     else if (x =="dmchord"){
         document.getElementById("frets").src = "./assets/imgs/dmchord.png";
+        dmSound.play();
     }
     else if (x =="emchord"){
         document.getElementById("frets").src = "./assets/imgs/emchord.png";
+        emSound.play();
     };
 
 
